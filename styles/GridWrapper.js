bb7503e672022faad07c3340bd770ffae7770331
Enrique Devars <e.devars.dev@gmail.com>
2020-04-13 18:25:41 -0500

    :sparkles: Responsive design

--- styles/GridWrapper.js
+++ styles/GridWrapper.js
@@ -1,26 +1,31 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const GridWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 450px;
+  display: grid;
+  grid-template-columns: 1fr 450px;
+  min-width: 100%;
 
-    aside{
-        background: #1d1a1a;
-        padding: 20px 45px;
-        height: calc(100vh - 40px);
+  aside {
+    background: #1d1a1a;
+    padding: 20px 45px;
+    height: calc(100vh - 40px);
 
-        p{
-            font-size: 21px;
-            text-align: justify;
-        }
-
-        h3{
-            font-size: 36px;
-        }
-
-        li{
-            margin-bottom: 10px;
-            font-size: 21px;
-        }
+    p {
+      font-size: 21px;
+      text-align: justify;
     }
-`
+
+    h3 {
+      font-size: 36px;
+    }
+
+    li {
+      margin-bottom: 10px;
+      font-size: 21px;
+    }
+  }
+
+  @media screen and (max-width: 720px) {
+    grid-template-columns: 1fr;
+  }
+`;
